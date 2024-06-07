bb07252f88ee97d8a945ba466ce5dde0b6572f5e
AshokBadade <146696860+AshokBadade@users.noreply.github.com>
2024-06-07 23:38:02 +0530

    comment out the code in index.js

--- index.js
+++ index.js
@@ -1,77 +1,77 @@
-const express = require('express') ;
-const mongoose = require('mongoose');
-const cors = require('cors');
-const { Schema } = mongoose;
+// const express = require('express') ;
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const { Schema } = mongoose;
 
-const app = express() ;
+// const app = express() ;
 
-const corsOptions = {
-    origin: "http://localhost:3000",
-    optionsSuccessStatus: 200 
-  };
+// const corsOptions = {
+//     origin: "http://localhost:3000",
+//     optionsSuccessStatus: 200 
+//   };
   
-app.use(express.json())
-app.use(cors(corsOptions));
+// app.use(express.json())
+// app.use(cors(corsOptions));
   
 
-main().catch(err => console.log(err));
+// main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb+srv://ASHOK:ASHOK@cluster0.ysj5m5f.mongodb.net/GoogleKeep?retryWrites=true&w=majority');
-  console.log("database conneted")
-}
+// async function main() {
+//   await mongoose.connect('mongodb+srv://ASHOK:ASHOK@cluster0.ysj5m5f.mongodb.net/GoogleKeep?retryWrites=true&w=majority');
+//   console.log("database conneted")
+// }
 
-const NotesSchema = new Schema({
-    "title" : {"type" : "String"} ,
-     "content" : {"type" : "String"}
-});
+// const NotesSchema = new Schema({
+//     "title" : {"type" : "String"} ,
+//      "content" : {"type" : "String"}
+// });
 
-const Notes =  new mongoose.model("Notes",NotesSchema) ;
+// const Notes =  new mongoose.model("Notes",NotesSchema) ;
 
-const n1 = new Notes({
-    "title": "first note" ,
-    "content" : "leraning mern to create project"
-});
+// const n1 = new Notes({
+//     "title": "first note" ,
+//     "content" : "leraning mern to create project"
+// });
 
-//n1.save() ;
+// //n1.save() ;
 
-app.get("/", (req, res) => {
-    res.send("hello");
-})
+// app.get("/", (req, res) => {
+//     res.send("hello");
+// })
 
-app.get("/get",cors(corsOptions),async(req,res) => {
-    try{
-      const docs = await Notes.find() ;
-      res.json(docs);
-    }
-    catch(err){  res.status(400).json(err)  } 
+// app.get("/get",cors(corsOptions),async(req,res) => {
+//     try{
+//       const docs = await Notes.find() ;
+//       res.json(docs);
+//     }
+//     catch(err){  res.status(400).json(err)  } 
      
-   } ) ;
+//    } ) ;
    
-   app.post("/post" ,cors(corsOptions),(req,res) => {
-    const p = new Notes(req.body) ;
-    p.save() ;
-    res.send("data added") ;
- })
+//    app.post("/post" ,cors(corsOptions),(req,res) => {
+//     const p = new Notes(req.body) ;
+//     p.save() ;
+//     res.send("data added") ;
+//  })
 
- app.delete("/del" , async(req,res) => {
-  const id = req.body.title ;
-  try{
-    const docs = await Notes.deleteMany({title:id}) ; 
-    res.json(docs)
-  }catch(err){ res.status(400).json(err) }
- })
+//  app.delete("/del" , async(req,res) => {
+//   const id = req.body.title ;
+//   try{
+//     const docs = await Notes.deleteMany({title:id}) ; 
+//     res.json(docs)
+//   }catch(err){ res.status(400).json(err) }
+//  })
 
 
- app.put("/put/:id" , async(req,res) => {
-  const id = req.params.id ;
-  console.log(id)
-  try{
-    const docs = await Products.findOneAndUpdate({title:id}, req.body ,{new:true}) ;
-    res.json(docs) ;
-  }catch(err){ res.status(400).json(err) }
- })
+//  app.put("/put/:id" , async(req,res) => {
+//   const id = req.params.id ;
+//   console.log(id)
+//   try{
+//     const docs = await Products.findOneAndUpdate({title:id}, req.body ,{new:true}) ;
+//     res.json(docs) ;
+//   }catch(err){ res.status(400).json(err) }
+//  })
  
 
 
-app.listen(3002)
+// app.listen(3002)
